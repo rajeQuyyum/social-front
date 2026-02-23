c980ebccd37e335b9eedb40b89d0c0f247ddf60c
Raje <rajevictory@gmail.com>
2026-02-23 18:23:59 +0100

    new

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='flex justify-between m-auto w-100 '>
+    <div className='flex justify-between m-auto w-100 bg-white '>
         <NavLink className="list-item text-red-400" to="/ad">profil edit admin</NavLink>
         <NavLink className="list-item text-blue-500" to="/igp">Profile</NavLink>
         <NavLink className="list-item text-yellow-500" to="/igm">message</NavLink>
